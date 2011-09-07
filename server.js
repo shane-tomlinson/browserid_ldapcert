32dbd35f7d604ef1bd75c11d4fc648a35391038d
Shane Tomlinson <stomlinson@mozilla.com>
2011-09-07 10:51:06 -0700

    Starting to get the basic flow down.

--- server.js
+++ server.js
@@ -1,47 +1,65 @@
 //#!/usr/bin/env node
 
 var express = require("express");
-var connect = require("connect");
+//var connect = require("connect");
 var MozillaLDAP = require("./auth");
 
 var app = express.createServer();
 var PORT = process.env.PORT || 80;
 
+/*
 app.use(
   connect.basicAuth(function(user, password, callback) {
-    MozillaLDAP.bind(user, password, function(err, creds) {
-      if (callback) {
-        callback(err, {
-          username: creds.username 
-        });
-      }
-    });
   })
 
 );
-
+*/
+app.use(express.bodyParser());
 app.use(app.router);
-app.use('/', express.errorHandler({ dump: true, stack: true }));
+app.use("/", express.errorHandler({ dump: true, stack: true }));
 
 app.use(
-  express.static(__dirname + '/static')
+  express.static(__dirname + "/static")
 );
 
-app.set('view options', {
+app.set("view options", {
   layout: true 
 });
 
-app.get('/', function(req, res, next) {
-  // req.remoteUser;
-  res.render('index.ejs', {
-    title: 'We give you certs!',
-    username: req.remoteUser.username
+app.get("/", function(req, res, next) {
+  res.render("index.ejs", {
+    title: "You need to authenticate"
+  });
+});
+
+app.post("/sign_in", function(req, res, next) {
+  console.log(req.body);
+  var body = req.body || {};
+  var username = body.username || "";
+  var password = body.password || "";
+
+  MozillaLDAP.bind(username, password, function(err, creds) {
+    var redirectTo = err ? "/" : "/authenticated";
+    res.redirect(redirectTo, 301);
+  });
+});
+
+app.get("/authenticated", function(req, res, next) {
+  res.render("authenticated.ejs", {
+    title: "Droppin' certs",
+    username: "stomlinson@mozilla.com"
   });
 });
 
 
+app.post("/sign_out", function(req, res, next) {
+  res.redirect("/");
+});
+
+
+
 app.listen(PORT, function () {
   var address = app.address();
-  fullServerAddress = address.address + ':' + address.port;
+  fullServerAddress = address.address + ":" + address.port;
   console.log("listening on " + fullServerAddress);
 });
